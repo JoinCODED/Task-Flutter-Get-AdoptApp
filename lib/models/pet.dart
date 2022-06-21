5a80ccb74fabe7cafc4633faceaff3ddea4f5b11
ZainabBaq <52189918+ZainabBaq@users.noreply.github.com>
2022-06-22 00:42:59 +0300

    Update pet.dart

--- lib/models/pet.dart
+++ lib/models/pet.dart
@@ -14,11 +14,5 @@ class Pet {
       required this.age,
       required this.gender});
 
-  Pet.fromJson(Map<String, dynamic> json)
-      : id = json['id'] as int?,
-        name = json['name'] as String,
-        adopted = json['adopted'] as bool,
-        image = json['image'] as String,
-        age = json['age'] as int,
-        gender = json['gender'] as String;
+  
 }
