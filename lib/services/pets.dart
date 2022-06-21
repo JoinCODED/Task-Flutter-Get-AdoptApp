88e9a6763e37ac73674bec75b11bc758a145c10a
ZainabBaq <52189918+ZainabBaq@users.noreply.github.com>
2022-06-22 00:44:53 +0300

    Update pets.dart

--- lib/services/pets.dart
+++ lib/services/pets.dart
@@ -4,12 +4,11 @@ import "package:dio/dio.dart";
 class DioClient {
   final Dio _dio = Dio();
 
-  final _baseUrl = 'https://coded-pets-api-crud.herokuapp.com/';
 
   Future<List<Pet>> getPets() async {
     List<Pet> pets = [];
     try {
-      Response response = await _dio.get(_baseUrl + '/pets');
+      Response response =  await _dio.get("https://coded-pets-api-crud.herokuapp.com/pets");
       pets = (response.data as List).map((pet) => Pet.fromJson(pet)).toList();
     } on DioError catch (error) {
       print(error);
